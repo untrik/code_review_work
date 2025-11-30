f0625c3ddaa611285643c00d9f9a668cd8144a57
untrik <zkirpa2005@mail.ru>
2025-11-30 20:29:55 +0300

    create Calculator.java and add tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0){
+            throw new IllegalArgumentException("the divisor cannot be equal to 0"); // кидаем Exception если делитель равен 0 (деление на 0 неопределено)
+        }
+        return a/b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+//        (x + 5) * 2 − (18 / 3)
+        int x = 3;
+        int part1 = add(x,5); // x + 5 = 8 при x = 3
+        int part2 = div(18,3); // 18 / 3 = 6
+        int mult = times(part1,2); // 8 * 2 = 16
+        int res = dif(mult,part2); //  16 - 6 = 10
+        return res;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(2, calculator.add(1, 1));
+        assertEquals(-8, calculator.add(-5, -3));
+        assertEquals(7, calculator.add(10, -3));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(13, calculator.dif(10, -3));
+        assertEquals(5, calculator.dif(10, 5));
+        assertEquals(-5, calculator.dif(5, 10));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(2, calculator.div(10, 5));
+        assertEquals(-2, calculator.div(-10, 5));
+        assertEquals(2, calculator.div(-10, -5));
+        assertThrows(IllegalArgumentException.class, () -> calculator.div(10, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(50, calculator.times(-10, -5));
+        assertEquals(-50, calculator.times(10, -5));
+        assertEquals(50, calculator.times(10, 5));
+        assertEquals(0, calculator.times(0, 5));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+//        (x + 5) * 2 − (18 / 3) при x = 3
+//        (3 + 5) * 2
+        assertEquals(10,calculator.solver());
     }
 }
