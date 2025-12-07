d39b24c81a3e51310a45af1037e82e7db7aa1e9d
untrik <150849382+untrik@users.noreply.github.com>
2025-12-07 22:57:54 +0300

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     Calculator calculator = new Calculator();
-
-    Calculator calculator = new Calculator();
     @Test
     void add() {
         assertEquals(2, calculator.add(1, 1));
